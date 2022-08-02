3a6c903cfb8e248d042ea732a6377ae1d762f7e8
shreyash <shreyashmachale@gmail.com>
2022-08-02 11:08:23 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import './index.css'
 import NavBar from './Components/common/Navbar/NavBar'
+import 'bootstrap/dist/css/bootstrap.css'
 function App() {
   return (
     <div className="App">
-  <NavBar/>
-Utility
+      <NavBar/>
+<div className={"container"}>
+<div>
+
+</div>
+</div>
     </div>
   );
 }
--- src/Components/common/Navbar/NavBar.js
+++ src/Components/common/Navbar/NavBar.js
@@ -9,22 +9,18 @@ const  NavBar =()=> {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">Simple Converter</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link href="#link">Tools</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.1">Image Converter</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Video Converter</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Audio Converter</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
