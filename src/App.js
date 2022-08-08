7e417b7621ad44a8ede3a0531331506dec91145c
akshit <akshitk540@gmail.com>
2022-08-08 07:54:38 +0530

    added empty file Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './index.css'
-import NavBar from './Components/common/Navbar/NavBar'
+import NavBar from './components/common/Navbar/NavBar'
 import 'bootstrap/dist/css/bootstrap.css'
+import FilesComponent from './components/common/FilesComponent/FilesComponent';
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <FilesComponent/>
 <div className={"container"}>
 <div>
 
---
+++ src/Components/common/FilesComponent/FilesComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Container from 'react-bootstrap/Container';
+
+const  FilesComponent =()=> {
+    return (
+       <></>
+    );
+}
+
+export default FilesComponent;
